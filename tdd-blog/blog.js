0047c3aa76d9be9069281c43e54d326858e2f92a
lorenalongoria <lorena.longoria.1@spaceforce.mil>
2025-04-15 14:44:33 -0400

    blog complete

--- tdd-blog/blog.js
+++ tdd-blog/blog.js
@@ -1,30 +1,71 @@
 class Blog {
   constructor() {
     this.posts = [];
+    this.comments = [];
   }
 
-  addBlogPost(post, author) {
-    this.posts.push({ post: post, author: author });
+  addBlogPost(title, author, genre) {
+    this.posts.push({ title: title, author: author, genre: genre });
+  }
+
+  addComment(title, user, comment) {
+    this.comments.push({ title: title, user: user, comment: comment });
+  }
+  
+  deleteBlog(title) {
+    this.posts = this.posts.filter(post => post.title !== title);
+    this.comments = this.comments.filter(comment => comment.title !== title);
   }
 
   getBlogPosts() {
-    return this.posts.length
-    === 0 ? "There are 0 blog posts"
-    : this.posts.map(Blog => Blog.post);
+    return this.posts.length === 0
+      ? "There are 0 blog posts"
+      : this.posts.map((Blog) => Blog.title);
+  }
+
+  getBlogComments(title) {
+    const BlogComments = this.comments.filter(
+      (commentObj) => commentObj.title === title
+    );
+    const formattedComments = BlogComments.map((commentObj) => {
+      return { user: commentObj.user, comment: commentObj.comment };
+    });
+    return formattedComments.length === 0
+      ? []
+      : [{ comments: formattedComments }];
+  }
+
+  getCommentsBy(user) {
+    const BlogComments = this.comments.filter(
+      (commentObj) => commentObj.user === user
+    );
+    const formattedComments = BlogComments.map((commentObj) => {
+      return { article: commentObj.title, comment: commentObj.comment };
+    });
+    return { user: user, comments: formattedComments };
   }
 
   getBlogAuthors() {
-    return this.posts
-    .filter(Blog => Blog.author)
-    .map(Blog => Blog.author)
+    return this.posts.filter((Blog) => Blog.author).map((Blog) => Blog.author);
   }
 
-  getSpecificBlogAuthor(post) {
-    let blog = this.posts.find(Blog => Blog.post === post);
+  getSpecificBlogAuthor(title) {
+    let blog = this.posts.find((Blog) => Blog.title === title);
     if (!blog) {
       return "No such blog post";
     }
     return blog.author ? blog.author : "Blog has no author";
+  }
+
+  getBlogsFromGenre(genre) {
+    return this.posts.filter((Blog) => Blog.genre === genre);
+  }
+
+  getAllGenres() {
+    let genres = this.posts.map((Blog) => Blog.genre);
+    return genres.length === 0
+      ? "There are no genres available"
+      : this.posts.map((Blog) => Blog.genre);
   }
 }
 
