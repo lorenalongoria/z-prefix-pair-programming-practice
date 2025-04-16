6790718b8a4692a161e8360626c10afa705b8182
lorenalongoria <lorena.longoria.1@spaceforce.mil>
2025-04-16 12:35:16 -0400

    solutions

--- tdd-calculator/calculator.js
+++ tdd-calculator/calculator.js
@@ -1,7 +1,72 @@
 class Calculator {
   constructor() {
-    
+    this.value = 0;
+  }
+
+  getCurrentState() {
+    return this.value;
+  }
+
+  addInputs(...nums) {
+    let sum = 0;
+    for (let i = 0; i < nums.length; i++) {
+      sum += nums[i];
+    }
+    this.value = sum;
+    return sum;
+  }
+
+  subtractInputs(...nums) {
+    if (nums.length === 0) return 0;
+    let result = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+      result -= nums[i];
+    }
+    return result;
+  }
+
+  multiplyInputs(...nums) {
+    let product = 1;
+    for (let i = 0; i < nums.length; i++) {
+      product *= nums[i];
+    }
+    return product;
+  }
+
+  divideInputs(...nums) {
+    if (nums.length === 0) return undefined;
+    let quotient = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+      quotient /= nums[i];
+    }
+    return quotient;
+  }
+
+
+  toPower(base, exponent) {
+    return Math.pow(base, exponent);
+  }
+
+
+  squareRoot(num) {
+    let root = Math.sqrt(num);
+    if (Number.isInteger(root)) {
+      return root;
+    } else {
+      return parseFloat(root.toFixed(2));
+    }
+  }
+
+
+  clearCurrentStatus() {
+    this.value = 0;
+    return true;
+  }
+
+
+  negPosReversal(num) {
+    return -num;
   }
 }
 
-module.exports = Calculator
+module.exports = Calculator;
--- tdd-set-javascript/set.js
+++ tdd-set-javascript/set.js
@@ -1,7 +1,30 @@
 class GalvanizeSet {
   constructor() {
+    this.items = [];
+  }
 
+  getSize() {
+    return this.items.length;
+  }
+
+  insert(item) {
+    if (!this.contains(item)) {
+      this.items.push(item);
+    }
+  }
+
+  contains(item) {
+    return this.items.includes(item);
+  }
+
+  delete(item) {
+    const index = this.items.indexOf(item);
+    if (index === -1) {
+      return false;
+    }
+    this.items.splice(index, 1);
+    return true;
   }
 }
 
-module.exports = GalvanizeSet
+module.exports = GalvanizeSet;
